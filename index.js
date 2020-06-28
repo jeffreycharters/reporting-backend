104a7bc7e37467e77e7af5ab6075adc69ef377a6
dietBlue <jeff.charters@gmail.com>
2020-06-28 12:23:00 -0400

    update for heroku

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors)
+
 app.get('/', (req, res) => {
   res.send('<h1>boo</h1>')
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
