3d03bc815cda22c17ba75fec903a24e952d7b8f3
dietBlue <jeff.charters@gmail.com>
2020-06-28 12:16:41 -0400

    new backend repo

---
+++ build/precache-manifest.9e8601854eaf0c9653b61e8698bcf580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbff3ee0ee9d16ea0db03216b6eba56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b52d88553d0ac93ab7f",
+    "url": "/static/css/main.e0350f89.chunk.css"
+  },
+  {
+    "revision": "7628aff98471c93b9a7a",
+    "url": "/static/js/2.dad6b060.chunk.js"
+  },
+  {
+    "revision": "f9e724c2ca2c2a35188e3463f2238bba",
+    "url": "/static/js/2.dad6b060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b52d88553d0ac93ab7f",
+    "url": "/static/js/main.d50e7df3.chunk.js"
+  },
+  {
+    "revision": "2228b862f07fb73749d3",
+    "url": "/static/js/runtime-main.d1300dc9.js"
+  }
+]);
